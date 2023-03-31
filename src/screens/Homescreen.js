f84db34a31899781d6f52361d75c6f0dca12b8d4
ArditaHoti <ardita.hoti8@gmail.com>
2023-03-31 17:49:35 +0200

    Initial App work

---
+++ src/screens/Homescreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { View,Text,StyleSheet } from "react-native";
+import { Icon } from "@rneui/base";
+import { colors, parameters } from "../global/style";
+
+
+export default function HomeHeader(){
+    return(
+    <View>
+        
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+    header: {
+        backgroundColor: colors.buttons,
+        height:parameters.headerHeight
+    }
+})
